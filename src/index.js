4d2a732bed665b2ba1720a51c40f054a6f2b9e84
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-11 20:25:16 +0530

    added signup component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import config from './aws-exports'
+import Amplify from 'aws-amplify'
 
+
+Amplify.configure(config)
 ReactDOM.render(<App />, document.getElementById('root'));
 
